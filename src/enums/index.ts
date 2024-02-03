cb00b49f7d2a5e218f458c93890694fc17758738
Ed-Games <edmarcosfilho2015@gmail.com>
2024-02-03 08:51:33 -0300

    feat: add enums for handling pomodoro time and mode values

---
+++ src/enums/index.ts
@@ -0,0 +1,11 @@
+export enum Mode {
+  POMODORO="Pomodoro",
+  SHORTBREAK="ShortBreak",
+  LONGBREAK="LongBreak"
+}
+
+export enum Time {
+  POMODORO= 25 * 60 ,
+  SHORTBREAK=5 * 60,
+  LONGBREAK= 15 * 60
+}
