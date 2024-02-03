956acd817d754c039c47d7d9882c4b5c4bc236d0
Ed-Games <edmarcosfilho2015@gmail.com>
2024-02-02 21:21:40 -0300

    feat: add loadUsersFromRoom, addUserToRoom methods and users state to hold the user list

--- src/hooks/useUserRoom.ts
+++ src/hooks/useUserRoom.ts
@@ -1,26 +1,49 @@
-import { IUser } from "@/interfaces/User"
-import { database } from "@/services/firebase"
-import { get, ref, remove } from "firebase/database"
-import { useSearchParams } from "next/navigation"
+import { IUser } from "@/interfaces/User";
+import { database } from "@/services/firebase";
+import { get, onValue, ref, remove, set } from "firebase/database";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export const useUserRoom = () => {
+  const [users, setUsers] = useState<IUser[]>();
   const searchParams = useSearchParams();
-  const addUserToRoom = (user: IUser) => {
+  const addUserToRoom = async (user: IUser) => {
+    const userRef = ref(
+      database,
+      `users/${searchParams.get("slug")}/${user.id}`
+    );
+    await set(userRef, user);
+  };
 
-  }
+  const loadUsersFromRoom = () => {
+    const userRef = ref(database, `users/${searchParams.get("slug")}`);
+    onValue(userRef, async (snapshot) => {
+      const rawUsers = snapshot.val();
+      const parsedUsers: IUser[] = Object.keys(rawUsers).map(
+        (key) => rawUsers[key as any]
+      );
 
-  const removeUserFromRoom = async(userId: string) => {
-    const userRef = ref(database, `users/${searchParams.get('slug')}/${userId}`);
+      setUsers(parsedUsers);
+    });
+  };
+
+  const removeUserFromRoom = async (userId: string) => {
+    const userRef = ref(
+      database,
+      `users/${searchParams.get("slug")}/${userId}`
+    );
     await remove(userRef);
-  }
+  };
 
-  const isUserInRoom = async(userId: string) => {
-    const userRef = ref(database, `users/${searchParams.get('slug')}/${userId}`);
+  const isUserInRoom = async (userId: string) => {
+    const userRef = ref(
+      database,
+      `users/${searchParams.get("slug")}/${userId}`
+    );
     const result = await get(userRef);
 
     return Boolean(result);
-  }
+  };
 
-
-  return {addUserToRoom, removeUserFromRoom, isUserInRoom}
-}
+  return { users, addUserToRoom, loadUsersFromRoom, removeUserFromRoom, isUserInRoom };
+};
