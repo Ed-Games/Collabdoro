fc3179cff313e8a9917e2523818d1eb0ac8eb3b7
Ed-Games <edmarcosfilho2015@gmail.com>
2024-02-05 11:17:10 -0300

    fix: replace searchParams with Params (from the route)

--- src/app/rooms/[slug]/page.tsx
+++ src/app/rooms/[slug]/page.tsx
@@ -11,33 +11,38 @@ import styles from "./styles.module.scss";
 import { BottomTabNavigator } from "./components/BottomTabNavigator";
 import { Loader } from "./components/Loader";
 import Header from "./components/Header";
-import { useSearchParams } from "next/navigation";
+import { useParams } from "next/navigation";
 import { Board } from "./Board";
 import { useUserRoom } from "@/hooks/useUserRoom";
 import Pomodoro from "./components/Pomodoro";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+
+interface IParams extends Params {
+  slug: string;
+}
 
 const Room: NextPage = () => {
   const { user } = useAuth();
   const router = useRouter();
-  const searchParams = useSearchParams();
+  const {slug} = useParams<IParams>();
   const { room, load, end } = useRoom();
   const { users, removeUserFromRoom } = useUserRoom();
 
   const closeRoom = () => {
     toast.warning("A sala foi encerrada");
-    end(searchParams.get("slug")!);
+    end(slug);
   };
+  
 
   useEffect(() => {
     if (user === undefined) return;
-
     if (!user || user.removed) {
       router.push("/");
       return;
     } else {
-      searchParams.get('slug') && load(searchParams.get("slug")!);
+      slug && load(slug);
     }
-  }, [load, router, searchParams, user]);
+  }, [load, router, slug, user]);
 
   return (
     <div id="room" className={`container ${styles.roomContainer}`}>
